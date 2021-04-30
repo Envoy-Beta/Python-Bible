f3c02519f52ed9e42c75c5df0394eeccdf5a504d
Envoy-Beta <83474429+Envoy-Beta@users.noreply.github.com>
2021-04-30 16:08:39 -0500

    Test

--- while_loops2.py
+++
@@ -1,9 +0,0 @@
-#List of 3 Names
-L = []
-
-while len(L) < 3:
-    new_name = input("Please add a new name: ").strip().capitalize()
-    L.append(new_name)
-
-print("Sorry, list is full.")
-print(L)
